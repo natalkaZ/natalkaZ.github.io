32e430ae5d31b433f77e9bae2b5bf4555ecff70c
natalkaZ <natalka.zakalyk@gmail.com>
2017-08-21 02:48:27 +0300

    Second commit
    js change

--- probujdenie/js/js.js
+++ probujdenie/js/js.js
@@ -1,7 +1,9 @@
-$('.sl').slick({
-  autoplay: true,
-  autoplaySpeed: 3000,
-  fade: true,
-  infinite: true,
-  arrows: false
+$(function(){
+  $('.sl').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    infinite: true,
+    arrows: false
+  });
 });
